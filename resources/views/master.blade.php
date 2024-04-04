721c2b6cd56d0471f5e946b3ede1502c6abaab4f
payal <payal@staqocom>
2024-04-04 12:51:41 +0530

    login page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Comm Project</title>
+    {{-- bootstrap --}}
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    {{-- jquery --}}
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    {{View::make('header')}}
+
+@yield('content')
+{{View::make('footer')}}
+
+</body>
+
+{{-- <script>
+    $(document).ready(function() {
+        $('button').click(function() {
+            // alert('all set')
+        })
+    })
+</script> --}}
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px;
+    }
+</style>
+
+</html>
